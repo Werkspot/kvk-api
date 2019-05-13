3ffb40c35a3b19919d692144ee5aeaf3e46d1583
Youri Westerman <tetracon@gmail.com>
2019-05-13 13:48:19 +0200

    Introduced interfaces for KvkClient and depending on QueryInterface instead of ProfileQuery

---
+++ src/KvkPaginatorAwareInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Werkspot\KvkApi;
+
+use Werkspot\KvkApi\Client\KvkPaginatorInterface;
+
+interface KvkPaginatorAwareInterface
+{
+    public function getNextPage(KvkPaginatorInterface $kvkPaginator): KvkPaginatorInterface;
+    public function getPreviousPage(KvkPaginatorInterface $kvkPaginator): KvkPaginatorInterface;
+}
